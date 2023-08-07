d65bf4ffcc1f7daf29f41fa68299b1528e0d1c31
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-08-07 15:20:19 -0700

    Cleaning UBC Grad Programs

---
+++ R/cleaning_ubc_grad_programs.R
@@ -0,0 +1,20 @@
+# Loading Packages =============================================================
+library(here)
+library(janitor)
+library(tidyverse)
+
+# Constants ====================================================================
+UBC_GRAD_PROGRAMS <- here("data", "ubc_grad_programs.csv")
+
+# Loading Data =================================================================
+programs <- read_csv(UBC_GRAD_PROGRAMS)
+
+# Cleaning UBC Grad Programs ===================================================
+cleaned_programs <- programs %>%
+  clean_names() %>%
+  extract(col = program_name, 
+          into = c("program_name", "degree_type"), 
+          regex = "(.*) \\((\\w+)\\)")
+
+# Saving Data ==================================================================
+write_csv(cleaned_programs, file = here("data", "cleaned_programs.csv"))
