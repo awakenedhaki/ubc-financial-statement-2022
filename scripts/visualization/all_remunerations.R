4833bfcb5469b48aad9d7bca81b7b250d9fb363c
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-08-07 20:51:54 -0700

    Saving figures

--- scripts/visualization/all_remunerations.R
+++ scripts/visualization/all_remunerations.R
@@ -1,5 +1,4 @@
 # Loading Libraries ============================================================
-library(uuid)
 library(here)
 library(scales)
 library(tidyverse)
@@ -9,10 +8,10 @@ update_geom_defaults("bar", list(color = "black", fill = "salmon"))
 theme_set(theme_minimal())
 
 # Constants ====================================================================
-DATA <- here("data")
+DATA <- here("data", "processed")
 
 # Loading Data =================================================================
-remunerations <- read_csv(here(DATA, "processed_remunerations.csv"))
+remunerations <- read_csv(here(DATA, "all_remunerations.csv"))
 
 # Visualizations ===============================================================
 # # Top 10 Remunerations
@@ -26,15 +25,20 @@ remunerations %>%
   head(n = 10)
 
 # # Distribution of Remuneration
-remunerations %>%
+remuneration_histogram <- remunerations %>%
   ggplot(aes(x = remuneration)) +
     geom_histogram() +
     geom_vline(aes(xintercept = median(remuneration))) +
     scale_x_continuous(breaks = seq(0, 1e6, 1e5), label = label_dollar()) +
     scale_y_continuous(label = label_comma())
 
+ggsave(filename = "all_remuneration_distribution.svg",
+       path = here("figures"),
+       plot = remuneration_histogram, device = "svg",
+       width = 4,  height = 10, units = "in", scale = 1, dpi = 300)
+
 # # Benford's Law
-remunerations %>%
+benford <- remunerations %>%
   mutate(remuneration = str_extract(remuneration, pattern = boundary("character"))) %>%
   count(first_digit = remuneration) %>%
   mutate(percent = n / sum(n)) %>%
@@ -43,3 +47,8 @@ remunerations %>%
     geom_text(aes(label = percent(percent, accuracy = 0.01)), vjust = -0.5) +
     scale_y_continuous(label = label_percent()) +
     expand_limits(y = c(0, .6))
+
+ggsave(filename = "all_remuneration_benford.svg",
+       path = here("figures"),
+       plot = benford, device = "svg",
+       width = 4,  height = 10, units = "in", scale = 1, dpi = 300)
