1932fccbc84da1c043a42bb1f15f31a932124443
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-08-11 22:47:08 -0700

    Adding documentation and two special cases.

--- cli/disambiguate_ubc_directory_results.py
+++ cli/disambiguate_ubc_directory_results.py
@@ -14,30 +14,78 @@ DISAMBIGUATED_EMPLOYEES = Path.cwd() / "data" / "tmp" / "disambiguated_employees
 
 # Helper Functions
 def load_ambiguous_employees():
+    """
+    Load ambiguous employee data from a JSON file.
+
+    Returns:
+        dict: A dictionary containing ambiguous employee data.
+
+    Description:
+        This function reads and loads data from the specified JSON file containing ambiguous employee information.
+        The loaded data is returned as a dictionary.
+
+    Note:
+        - The JSON file path is specified by the global variable `AMBIGUOUS_EMPLOYEES`.
+    """
     with AMBIGUOUS_EMPLOYEES.open(mode="r") as f:
         return json.load(f)
 
 
 def save_json(path, data):
+    """
+    Save data to a JSON file.
+
+    Args:
+        path (Path): The file path where the JSON data will be saved.
+        data (Any): The data to be saved as JSON.
+
+    Description:
+        This function saves the provided data in JSON format to the specified file path.
+    """
     with path.open(mode="w") as f:
         json.dump(data, f)
 
 
 def display_results(results):
+    """
+    Display enumerated UBC directory search results.
+
+    Args:
+        results (list): A list of search results.
+
+    Description:
+        This function displays search results along with corresponding index numbers for selection.
+    """
     for i, result in enumerate(results):
         result = re.sub(pattern=r"[\s]{2,}", repl=" ", string=" ".join(result[:2]))
         click.echo(message=f"{i}: {result}")
 
 
 def select_retained_result(ambiguous_employees):
+    """
+    Select employee results to be retained.
+
+    Args:
+        ambiguous_employees (dict): A dictionary containing ambiguous employee data.
+
+    Returns:
+        dict: A dictionary mapping employee names to retained results.
+
+    Description:
+        This function prompts the user to select retained results for each ambiguous employee entry.
+        The user can choose between no match, multiple possible matches, or specific search results.
+    """
     retained_results = {}
     for name, results in ambiguous_employees.items():
         click.echo(message=click.style(name, fg="green"))
         display_results(results)
         index = click.prompt("Which entry do you want to retain?", type=int)
-        if index < 0:
-            retained_results[name] = ""
-        retained_results[name] = results[index]
+        if index == -1:  # No match
+            retained_results[name] = "No match"
+        elif index == -2:  # Multiple possible matches
+            retained_results[name] = "Multiple possible matches"
+        else:
+            retained_results[name] = results[index]
         click.clear()
 
 
