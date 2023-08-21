52a7146bbaaa8ef112c45752ccc6e9f94d6e17b8
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-08-21 13:54:41 -0700

    Black formatting and clearing previous message

--- cli/resolve_floating_names_from_financial_report.py
+++ cli/resolve_floating_names_from_financial_report.py
@@ -5,9 +5,10 @@ import pandas as pd
 from pathlib import Path
 
 # Constants
-DATA = Path(__file__).parents[1] / "data" 
+DATA = Path(__file__).parents[1] / "data"
 REMUNERATION = DATA / "processed" / "all_remunerations.csv"
 TMP = DATA / "tmp"
+
 
 # Helper Functions
 def load_ambiguous_names(path):
@@ -22,11 +23,14 @@ def load_ambiguous_names(path):
 @click.command()
 def main():
     ambiguous_names = load_ambiguous_names(DATA)
-    print(ambiguous_names)
     ambiguous_names["flag"] = 0
     index = 0
     while index <= ambiguous_names.shape[0]:
-        click.echo(message=click.style(ambiguous_names.loc[index, "ambiguous_name"], fg="green"))
+        click.echo(
+            message=click.style(
+                ambiguous_names.loc[index, "ambiguous_name"], fg="green"
+            )
+        )
         click.echo(message=f"0. {ambiguous_names.loc[index, 'name']}")
         click.echo(message=f"1. {ambiguous_names.loc[index + 1, 'name']}")
         name_assignment = click.prompt("Assign name [-1/0/1]", type=int)
@@ -36,7 +40,7 @@ def main():
         else:
             ambiguous_names.loc[index + name_assignment, "flag"] = True
         index += 2
-    
+        click.clear()
 
 
 if __name__ == "__main__":
