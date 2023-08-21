4718580a98f10cfad4924f38179074ff37504001
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-08-21 12:51:19 -0700

    CLI for resolving ambiguous names from the financial report parsing

--- cli/resolve_floating_names_from_financial_report.py
+++ cli/resolve_floating_names_from_financial_report.py
@@ -2,24 +2,42 @@
 import click
 import pandas as pd
 
-from uuid import uuid4
 from pathlib import Path
 
 # Constants
-DATA = Path(__file__).parents[2] / "data" / "all_remunerations.csv"
-
+DATA = Path(__file__).parents[1] / "data" 
+REMUNERATION = DATA / "processed" / "all_remunerations.csv"
+TMP = DATA / "tmp"
 
 # Helper Functions
-def load_unresolved_rows(path):
-    df = pd.read_csv(DATA)
-    return df[~df["ambiguous_name"].isnull()]
+def load_ambiguous_names(path):
+    df = pd.read_csv(REMUNERATION)
+    ambiguous_names = df[~df["ambiguous_name"].isnull()].drop(
+        columns=["remuneration", "expenses"]
+    )
+    return ambiguous_names.reset_index()
 
 
 # Main Functions
 @click.command()
 def main():
-    pass
+    ambiguous_names = load_ambiguous_names(DATA)
+    print(ambiguous_names)
+    ambiguous_names["flag"] = 0
+    index = 0
+    while index <= ambiguous_names.shape[0]:
+        click.echo(message=click.style(ambiguous_names.loc[index, "ambiguous_name"], fg="green"))
+        click.echo(message=f"0. {ambiguous_names.loc[index, 'name']}")
+        click.echo(message=f"1. {ambiguous_names.loc[index + 1, 'name']}")
+        name_assignment = click.prompt("Assign name [-1/0/1]", type=int)
+        if name_assignment == -1:
+            index += 2
+            continue
+        else:
+            ambiguous_names.loc[index + name_assignment, "flag"] = True
+        index += 2
+    
 
 
 if __name__ == "__main__":
-    pass
+    main()
